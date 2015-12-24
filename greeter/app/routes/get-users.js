60ec7623e9b7eaedf28d91f9f1ad482a2b29a0a4
nrhubbar <14hubbardn@gmail.com>
2015-12-24 10:32:47 -0500

    renaming the files to names that will make more sense

--- greeter/app/routes/get-users.js
+++ greeter/app/routes/get-users.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    console.log(Ember.$.get('Users.json'));
-    return Ember.$.get('Users.json');
+    return Ember.$.get('/api/v1/users.json');
   }
 });
