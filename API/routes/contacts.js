4bc8a1c54f1ac19516c299e46fa41d2d1b10ce12
Danny Peck <djpeck@Dannys-MacBook-Pro.local>
2016-01-23 14:59:25 -0500

    Mongoose queries for get requests working

--- API/routes/contacts.js
+++ API/routes/contacts.js
@@ -1,5 +1,39 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var db = mongoose.connection;
+
+db.on('error', console.error);
+
+var contactSchema = new mongoose.Schema({
+    _id : Number,
+    firstname : String,
+    lastname : String,
+    number : String,
+    location : String
+    }, 
+    {versionKey : false}
+);
+
+var Contact = mongoose.model('Contact', contactSchema);
+
+/*var danny = new Contact({
+    _id : 0,
+    firstname : "Danny",
+    lastname : "Peck",
+    number : "3175026963",
+    location : "Indianapolis, Indiana"
+});
+
+danny.save(function (err, danny) {
+    if (err) {
+        return console.error(err);
+    }
+    console.dir(danny.toObject());
+});*/
+
+mongoose.connect('localhost:27017');
 
 var router = express.Router();
 router.use(bodyParser.json());
@@ -11,6 +45,14 @@ router.use(bodyParser.json());
 // http://127.0.0.1:3000/contacts?firstname=nick&lastname=hubbard&number=0123456
 
 router.get('/', function(req, res, next) {
+    Contact.findOne({firstname : req.query.firstname, lastname : req.query.lastname}, function (err, c) {
+	if (err) {
+	    return console.error(err);
+	}
+
+	console.dir(c.toObject());
+    });
+    
     console.log('firstname: ' + req.query.firstname 
             + ' '
             + 'lastname: ' + req.query.lastname
@@ -28,9 +70,10 @@ router.get('/', function(req, res, next) {
 //    console.log('name: ' + req.query.name + ' ' + 'number: ' + req.query.number);
 //    res.send('name: ' + req.query.name
 
-//router.post('/', function(req, res, next) {
-//    console.log(req.body.name);
-//    res.send(req.body.name);
-//});
+router.post('/', function(req, res, next) {
+    
+    //console.log(req.body.name);
+    //res.send(req.body.name);
+});
 
 module.exports = router;
