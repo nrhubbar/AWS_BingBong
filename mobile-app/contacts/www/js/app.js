9a8adaa491350bee9561797405f3df9965cfef9f
nrhubbar <14hubbardn@gmail.com>
2016-01-23 15:41:22 -0500

    Finnished the ablility to add users to a contact list on the client side

--- mobile-app/contacts/www/js/app.js
+++ mobile-app/contacts/www/js/app.js
@@ -1,37 +1,15 @@
-// Ionic Starter App
+angular.module('ionicApp', ['ionic'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+.controller('MyCtrl', function($scope, $ionicPopup) {
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+ $scope.contacts =[{firstName:"Nick",lastName:"Hubbard",location:"Indy"}];//$http.get(publicDns + '/api/v1/contacts');
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-
-  });
-})
-
-.controller('contact-list', function($scope, $ionicPopup){
-  $scope.contacts = $http.get(publicDns + '/api/v1/contacts');
-
-  $scope.createContact = function(){
+  $scope.createContact = function() {
     var newContact = {};
+    console.log("Hey");
 
-    $ionicPopup.show({
-      template : '<form><input type="text" placeholder="Name" ng-model="newContact.name"> <br> <input type="text" placeholder="Phone Number" ng-model="newContact.phoneNumber"> <br> <input type="text" placeholder="Location" ng-model="newContact.location"></form>',
+    var myPopup = $ionicPopup.show({
+      template : '<form name="myForm"><input type="text" placeholder="First Name" ng-model="newContact.firstName" name="firstName"> <br> <input type="text" placeholder="Last Name" ng-model="newContact.lastName" name="lastName"> <br> <input type="text" placeholder="Location" ng-model="newContact.location" name="location"></form>',
       title : 'New Contact',
       scope : $scope,
       buttons : [
@@ -40,14 +18,17 @@ angular.module('starter', ['ionic'])
             text : 'Save',
             type : 'button-positive',
             onTap : function(e){
-              $http.post(publicDns + '/api/v1/contacts', newContact);
+              //$http.post(publicDns + '/api/v1/contacts', newContact);
+              console.log(myForm.firstName.value);
+              $scope.contacts.push({firstName:myForm.firstName.value,lastName:myForm.lastName.value, location:myForm.location.value});
             }
         }
       ]
     })
-    .then(function($scope){
-      $scope.contacts = $http.get(publicDns + '/api/v1/contacts');
+    /*.then(function($scope){
+      //$scope.contacts = $http.get(publicDns + '/api/v1/contacts');
     });
+    */
 
   }
-})
+});
