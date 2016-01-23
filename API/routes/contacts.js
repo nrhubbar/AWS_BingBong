be63c6c34e0445dcc86df3c19bcaf237e3148122
Danny Peck <djpeck@Dannys-MacBook-Pro.local>
2016-01-23 15:26:05 -0500

    Can now queries the database and insert into the database via get and post requests

--- API/routes/contacts.js
+++ API/routes/contacts.js
@@ -4,35 +4,23 @@ var mongoose = require('mongoose');
 
 var db = mongoose.connection;
 
+// if there is an error in connecting to the db, print the error
 db.on('error', console.error);
 
+// creating the document schema for the contact objects
 var contactSchema = new mongoose.Schema({
-    _id : Number,
     firstname : String,
     lastname : String,
     number : String,
     location : String
     }, 
-    {versionKey : false}
+    {versionKey : false} // stops the return of the versionKey in queries
 );
 
+// creates the Contact object model based off the schema
 var Contact = mongoose.model('Contact', contactSchema);
 
-/*var danny = new Contact({
-    _id : 0,
-    firstname : "Danny",
-    lastname : "Peck",
-    number : "3175026963",
-    location : "Indianapolis, Indiana"
-});
-
-danny.save(function (err, danny) {
-    if (err) {
-        return console.error(err);
-    }
-    console.dir(danny.toObject());
-});*/
-
+// connects to the mongodb server (currently localhost)
 mongoose.connect('localhost:27017');
 
 var router = express.Router();
@@ -44,21 +32,19 @@ router.use(bodyParser.json());
 // an example GET request on the URL
 // http://127.0.0.1:3000/contacts?firstname=nick&lastname=hubbard&number=0123456
 
+// returns the result set to the client
 router.get('/', function(req, res, next) {
+    // queries the contacts collection for a contact with the given information 
     Contact.findOne({firstname : req.query.firstname, lastname : req.query.lastname}, function (err, c) {
 	if (err) {
 	    return console.error(err);
 	}
-
+	
+	// prints the result of the query to the console
 	console.dir(c.toObject());
     });
     
-    console.log('firstname: ' + req.query.firstname 
-            + ' '
-            + 'lastname: ' + req.query.lastname
-            + ' '
-            + 'number: ' + req.query.number);
-
+    // sends response message to the client (we want to send the result set
     res.send('firstname: ' + req.query.firstname
             + ' '
             + 'lastname: ' + req.query.lastname
@@ -66,14 +52,25 @@ router.get('/', function(req, res, next) {
             + 'number: ' + req.query.number);
 });
 
-//router.get('/:id', function(req, res, next) {
-//    console.log('name: ' + req.query.name + ' ' + 'number: ' + req.query.number);
-//    res.send('name: ' + req.query.name
-
+// commits the given Contact data into the database
 router.post('/', function(req, res, next) {
+    // creates the contact object based on passed in information
+    var contact = new Contact({
+	firstname : req.query.firstname,
+	lastname : req.query.lastname,
+	number : req.query.number,
+	location : req.query.location
+    });
+
+    // stores the contact object in the database
+    contact.save(function (err) {
+	if (err) {
+	    return console.error(err);
+	}
+    });
     
-    //console.log(req.body.name);
-    //res.send(req.body.name);
+    // sends a message back to the client
+    res.send(req.body.firstname);
 });
 
 module.exports = router;
