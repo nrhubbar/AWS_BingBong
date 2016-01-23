300df01ffe41e37ec4ae2b83df8d9160ffa92594
Josh <jogould@umail.iu.edu>
2016-01-23 12:39:16 -0600

    pushin

--- API/routes/contacts.js
+++ API/routes/contacts.js
@@ -7,14 +7,30 @@ router.use(bodyParser.json());
 // to access the form data for a post, use req.body
 // to access the form data for a get, use req.query
 
+// an example GET request on the URL
+// http://127.0.0.1:3000/contacts?firstname=nick&lastname=hubbard&number=0123456
+
 router.get('/', function(req, res, next) {
-    console.log(req.query.name);
-    res.send(req.query.name);
+    console.log('firstname: ' + req.query.firstname 
+            + ' '
+            + 'lastname: ' + req.query.lastname
+            + ' '
+            + 'number: ' + req.query.number);
+
+    res.send('firstname: ' + req.query.firstname
+            + ' '
+            + 'lastname: ' + req.query.lastname
+            + ' '
+            + 'number: ' + req.query.number);
 });
 
-router.post('/', function(req, res, next) {
-    console.log(req.body.name);
-    res.send(req.body.name);
-});
+//router.get('/:id', function(req, res, next) {
+//    console.log('name: ' + req.query.name + ' ' + 'number: ' + req.query.number);
+//    res.send('name: ' + req.query.name
+
+//router.post('/', function(req, res, next) {
+//    console.log(req.body.name);
+//    res.send(req.body.name);
+//});
 
 module.exports = router;
