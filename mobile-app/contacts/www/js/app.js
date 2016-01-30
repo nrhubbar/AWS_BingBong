a22fc2d48e669500d0df1883ffd6429f0dea33d8
nrhubbar <14hubbardn@gmail.com>
2016-01-30 15:58:01 -0500

    Working on functionality to connect to the api

--- mobile-app/contacts/www/js/app.js
+++ mobile-app/contacts/www/js/app.js
@@ -2,33 +2,36 @@ angular.module('ionicApp', ['ionic'])
 
 .controller('MyCtrl', function($scope, $ionicPopup) {
 
- $scope.contacts =[{firstName:"Nick",lastName:"Hubbard",location:"Indy"}];//$http.get(publicDns + '/api/v1/contacts');
+  $scope.contacts = $http.get('52.11.192.16/contacts');
+  $scope.contacts.push({firstName:"test", lastName:"testTest", number:"123"});
 
   $scope.createContact = function() {
     var newContact = {};
     console.log("Hey");
 
     var myPopup = $ionicPopup.show({
-      template : '<form name="myForm"><input type="text" placeholder="First Name" ng-model="newContact.firstName" name="firstName"> <br> <input type="text" placeholder="Last Name" ng-model="newContact.lastName" name="lastName"> <br> <input type="text" placeholder="Location" ng-model="newContact.location" name="location"></form>',
-      title : 'New Contact',
-      scope : $scope,
-      buttons : [
-        {text : 'Cancel'},
-        {
-            text : 'Save',
-            type : 'button-positive',
-            onTap : function(e){
-              //$http.post(publicDns + '/api/v1/contacts', newContact);
-              console.log(myForm.firstName.value);
-              $scope.contacts.push({firstName:myForm.firstName.value,lastName:myForm.lastName.value, location:myForm.location.value});
-            }
-        }
-      ]
-    })
-    /*.then(function($scope){
-      //$scope.contacts = $http.get(publicDns + '/api/v1/contacts');
-    });
-    */
+        template: '<form name="myForm"><input type="text" placeholder="First Name" ng-model="newContact.firstname" name="firstname"> <br> <input type="text" placeholder="Last Name" ng-model="newContact.lastname" name="lastname"> <br> <input type="text" placeholder="Number" ng-model="newContact.number" name="number"></form>',
+        title: 'New Contact',
+        scope: $scope,
+        buttons: [{
+          text: 'Cancel'
+        }, {
+          text: 'Save',
+          type: 'button-positive',
+          onTap: function(e) {
+            console.log(myForm.firstname.value);
 
-  }
+            var contact = {
+              firstname: myForm.firstname.value,
+              lastname: myForm.lastname.value,
+              number: myForm.number.value
+            });
+            $http.post('52.11.192.16/contacts', contact);
+          }
+        }]
+      })
+      .then(function($scope){
+        $scope.contacts = $http.get('52.11.192.16/contacts');
+      });
+    }
 });
