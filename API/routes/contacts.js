540eef289442a578ecf6217aeb1956f65a2a92b2
Danny Peck <djpeck@Dannys-MacBook-Pro.local>
2016-02-06 14:12:50 -0500

    Every night in my dreams, i see you, i feel you

--- API/routes/contacts.js
+++ API/routes/contacts.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+
 var router = express.Router();
-var jsonParser = bodyParser.json();
+var jsonParser =bodyParse.json();
+
+var mongoose = require('mongoose');
 var db = mongoose.connection;
 var cuid = require('cuid');
 
@@ -23,49 +25,7 @@ var contactSchema = new mongoose.Schema({
 // creates the Contact object model based off the schema
 var Contact = mongoose.model('Contact', contactSchema);
 
-<<<<<<< HEAD
 mongoose.connect('52.33.142.91:27017');
-=======
-// connects to the mongodb server (currently localhost)
-mongoose.connect('localhost:27017');
-
-var router = express.Router();
-router.use(bodyParser.json());
-
-// to access the form data for a post, use req.body
-// to access the form data for a get, use req.query
-
-// an example GET request on the URL
-// http://127.0.0.1:3000/contacts?firstname=nick&lastname=hubbard
-
-// returns the result set to the client
-router.get('/', function(req, res, next) {
-    // queries the contacts collection for a contact with the given information
-    Contact.findOne({firstname : req.query.firstname, lastname : req.query.lastname}, function (err, result) {
-        if (err) return console.error(err);
-        res.json(result);
-    });
-});
-
-// commits the given Contact data into the database
-router.post('/', function(req, res, next) {
-    // creates the contact object based on passed in information
-    var contact = new Contact({
-        firstname : req.body.firstname,
-        lastname : req.body.lastname,
-        number : req.body.number,
-        loc : req.body.loc
-    });
-
-    // stores the contact object in the database
-    contact.save(function (err) {
-        if (err) return console.error(err);
-    });
-
-    // we have to finish the post => we send an empty json response
-    res.json();
-mongoose.connect('52.88.203.125:27017');
->>>>>>> 908cc24d79d613e60f64911aa10eacae2d09bf01
 
 // returns the result set to the client
 router.get('/', function(req, res, next) {
